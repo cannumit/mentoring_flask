546227d418a7fa2195e60261096476e50efeb42d
cannumit <cannumit@gmail.com>
2025-03-10 15:45:51 +0100

    and

--- flask-_01_02_Jinja_Template/flask-01-hello-world-app/hello-world-app.py
+++ flask-_01_02_Jinja_Template/flask-01-hello-world-app/hello-world-app.py
@@ -14,5 +14,7 @@ app = Flask(__name__)
 
 if __name__ == '__main__':
 
-     # app.run(debug=True)
-     app.run(host= '0.0.0.0', port=80)
+  # app.run(debug=True)
+ app.run(host= '0.0.0.0', port=80)
+ and
+ 
